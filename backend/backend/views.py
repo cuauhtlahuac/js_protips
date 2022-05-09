0ff9a1acb297bfafce304f548bf0e47beb06d437
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-05-09 08:55:42 -0500

    feat: addign a new routes and views for the pages

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Context, loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+  return HttpResponse("HOME")
+  #return render(request, 'app/index.html')
 # Create your views here.
